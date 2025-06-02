2e8dc9f4ec11dbbb1464665dfd637f3b44381eb0
pppp <pppp@ppppui-MacBookPro.local>
2025-06-02 17:11:38 +0900

    feat: webview to native postmessage

--- screens/search/index.tsx
+++ screens/search/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dimensions, Platform, SafeAreaView } from "react-native";
+import { Alert, Dimensions, Platform, SafeAreaView } from "react-native";
 import WebView, { WebViewMessageEvent } from "react-native-webview";
 
 export default function SearchScreen() {
@@ -14,13 +14,22 @@ export default function SearchScreen() {
   const onMessage = (event: WebViewMessageEvent) => {
     const data = JSON.parse(event.nativeEvent.data);
 
-    if (data.type === "GET_DEVICE_INFO") {
-      webViewRef.current?.postMessage(
-        JSON.stringify({
-          type: "GET_DEVICE_INFO",
-          data: deviceInfo,
-        })
-      );
+    switch (data.type) {
+      case "GET_DEVICE_INFO":
+        webViewRef.current?.postMessage(
+          JSON.stringify({
+            type: "GET_DEVICE_INFO",
+            data: deviceInfo,
+          })
+        );
+        break;
+
+      case "SUBMIT_FORM":
+        Alert.alert("Form Submitted", JSON.stringify(data.payload, null, 2));
+        break;
+
+      default:
+        console.warn("⚠️ 알 수 없는 메시지 타입:", data.type);
     }
   };
 
@@ -30,6 +39,7 @@ export default function SearchScreen() {
     <SafeAreaView style={{ flex: 1 }}>
       <WebView
         ref={webViewRef}
+        keyboardDisplayRequiresUserAction={false}
         source={{ uri: "http://172.30.1.79:3000" }}
         onMessage={onMessage}
       />
